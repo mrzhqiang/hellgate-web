0aa8112da9b4ac26542604c018a5d9fb3b00c2d5
张强 <qiang.zhang@touchair.cn>
2019-09-04 09:43:12 +0800

    修复：首页背景图切换问题

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {animate, keyframes, state, style, transition, trigger} from '@angular/animations';
-import {interval} from 'rxjs';
+import {interval, Subscription} from 'rxjs';
 
+/* 最少 2 张图片，默认显示最后一张，然后再从第一张开始，循环播放。 */
 const IMAGES = [
   'assets/image/dyzm.jpg',
   'assets/image/dyzm2.jpg'
@@ -24,18 +25,24 @@ const IMAGES = [
     ])
   ]
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   title = '地狱之门';
   imageIndex = 0;
-  backImage = '';
+  backImage = IMAGES[IMAGES.length - 1];
 
   constructor() {
   }
 
+  private subscription: Subscription;
+
   ngOnInit() {
-    interval(3000).subscribe(value => {
-      this.imageIndex = value % 2;
+    this.subscription = interval(3000).subscribe(value => {
+      this.imageIndex = value % IMAGES.length;
       this.backImage = IMAGES[this.imageIndex];
     });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
